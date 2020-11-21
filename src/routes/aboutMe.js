0ce33a46133f9eb51a20461c863f74be53b168ae
Miura <tadamiura@gmail.com>
2020-11-21 16:31:44 +0100

    added a route to display only contact links

--- src/routes/aboutMe.js
+++ src/routes/aboutMe.js
@@ -16,4 +16,18 @@ router.get('/', (req,res) => {
     })
 })
 
+router.get('/links', (req,res) => {
+    const sql =
+    `SELECT github_profil, linkedin_profil, email 
+    FROM about_me`
+    connection.query(sql, (err, result) => {
+        if (err) {
+            console.log(err.message)
+            res.status(500).send('Erreur dans la récupération des informations about_me')
+        } else {
+            res.send(result)
+        }
+    })
+})
+
 module.exports = router
