afa5dedd605cd3af289032ea7d3ab98e82502282
tadamiura <tadamiura@gmail.com>
2023-01-26 12:14:59 +0100

    created routes experiences & certifications

---
+++ src/routes/experiences.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const { connection } = require('../helper/conf')
+const router = express.Router()
+
+router.get('/', (req, res) => {
+    const sql = `SELECT * FROM experience`
+    connection.query(sql, (err, result) => {
+        if (err) {
+            res.status(500).send('Erreur dans la récupération des expérience')
+        } else {
+            res.send(result)
+        }
+    })
+})
+
+module.exports = router;
